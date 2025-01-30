2d4b3bb8f7c6760c25dcff6f3ccc4296594f8f57
muzzammil-123 <itsiamkode@gmail.com>
2025-01-31 03:17:59 +0500

    Final Commit

--- server/src/index.js
+++ server/src/index.js
@@ -48,7 +48,4 @@ connectDB().then(() => {
   app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port " + process.env.PORT || 3000);
   });
-  app.get('/', (req, res)=>{
-    res.send("Helooooo")
-  })
 });
--- server/src/routes/PostsRoutes.js
+++ server/src/routes/PostsRoutes.js
@@ -2,7 +2,10 @@ import { Router } from "express";
 import { createPost, getAllPosts } from "../controller/PostsController.js";
 
 const router = Router()
-
+router.get('/testing', ()=>{
+    console.log('Helllllooo World')
+})
 router.get('/', getAllPosts)
 router.post('/', createPost)
+
 export default router
