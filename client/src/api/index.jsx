885dc2bb5b900579cdf27e1d995cccb23e65f9df
muzzammil-123 <itsiamkode@gmail.com>
2025-01-31 04:08:24 +0500

    FInal Commit

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,9 +1,7 @@
-import axios from 'axios'
-
 const API = axios.create({
-    baseURL : 'https://aiimagica-backend.vercel.app/api/',
-})
+  baseURL: '/api',  // Use relative path, Vercel handles the routing
+});
 
-export const GetPosts = async () => await API.get('/posts/')
-export const CreatePost = async (data) => await API.post('/posts/', data)
-export const GenerateImage = async (data) => await API.post('/generateImage/', data)
+export const GetPosts = async () => await API.get('/posts/');
+export const CreatePost = async (data) => await API.post('/posts/', data);
+export const GenerateImage = async (data) => await API.post('/generateImage/', data);
