47ad7c75d38da6e64ab14bba82c7d3520d69ec7e
muzzammil-123 <itsiamkode@gmail.com>
2025-01-31 02:11:48 +0500

    Update index.jsx

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -4,6 +4,6 @@ const API = axios.create({
     baseURL : 'https://aiimagica-backend.vercel.app/api/',
 })
 
-export const GetPosts = async () => await API.get('/posts/')
-export const CreatePost = async (data) => await API.post('/posts/', data)
-export const GenerateImage = async (data) => await API.post('/generateImage/', data)
+export const GetPosts = async () => await API.get('/posts')
+export const CreatePost = async (data) => await API.post('/posts', data)
+export const GenerateImage = async (data) => await API.post('/generateImage', data)
