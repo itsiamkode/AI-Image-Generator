a598819967034110e3b19daf0711d7ca56ad9249
muzzammil-123 <itsiamkode@gmail.com>
2025-01-31 18:58:30 +0500

    Final Commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api' : {
-        target: 'https://aiimagica-backend.vercel.app',  // Your backend URL
-        changeOrigin: true,  // Ensures the host header is modified for the target domain
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  },
   plugins: [react(), tailwindcss()],
-})
+});
